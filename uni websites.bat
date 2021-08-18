79d3a6cd3a5747f68ba3b61ddcc6db934cf2b54c
kleenkanteen <test@gmail.com>
2021-08-18 05:53:22 -0400

    Create uni websites.bat
    Batch files to open uni related sites

---
+++ uni websites.bat
@@ -0,0 +1,9 @@
+@echo off
+cls
+start "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" chrome "https://webmail.utoronto.ca"
+
+start "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" chrome "https://q.utoronto.ca/"
+
+start "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" chrome "https://mcs.utm.utoronto.ca/~209/index.shtml"
+
+exit
